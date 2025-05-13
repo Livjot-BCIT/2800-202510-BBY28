606fd7eb5618dc8f9fb84a59beff105fcf931a8c
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2025-05-13 14:04:45 -0700

    Added database connection code and .gitignore file

---
+++ index.js
@@ -0,0 +1,65 @@
+require("./utils.js");
+
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const Joi = require("joi");
+
+
+const expireTime = 24 * 60 * 60 * 1000; //expires after 1 day  (hours * minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority&tls=true`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+// Middleware 
+app.use((req, res, next) => {
+	res.locals.navLinks = [
+		{name: "Members", link: "/members"},
+		{name: "About", link: "/about"},
+		{name: "Contact", link: "/contact"},
+		{name: "Admin", link: "/admin"},
+		{name: "404", link: "/dne"}
+    ];
+
+    res.locals.isAuthenticated = req.session?.authenticated || false; // Set isAuthenticated for all templates
+	res.locals.currentPath = req.path; // Set currentPath globally
+    next();
+});
