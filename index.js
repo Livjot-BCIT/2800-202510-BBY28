0be640d24f5d468e266eec678c73b306cd3d857f
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2025-05-13 15:51:20 -0700

    Converted HTML files to EJS files and implemented a temporary database for testing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 require("./utils.js");
 
 require('dotenv').config();
@@ -26,7 +27,7 @@ const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
-var {database} = include('databaseConnection');
+const {database} = include('databaseConnection');
 
 const userCollection = database.db(mongodb_database).collection('users');
 
@@ -49,17 +50,99 @@ app.use(session({
 }
 ));
 
-// Middleware 
-app.use((req, res, next) => {
-	res.locals.navLinks = [
-		{name: "Members", link: "/members"},
-		{name: "About", link: "/about"},
-		{name: "Contact", link: "/contact"},
-		{name: "Admin", link: "/admin"},
-		{name: "404", link: "/dne"}
-    ];
+app.get('/nosql-injection', async (req,res) => {
+	var username = req.query.user;
 
-    res.locals.isAuthenticated = req.session?.authenticated || false; // Set isAuthenticated for all templates
-	res.locals.currentPath = req.path; // Set currentPath globally
+	if (!username) {
+		res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
+		return;
+	}
+	console.log("user: "+username);
+
+	const schema = Joi.string().max(20).required();
+	const validationResult = schema.validate(username);
+
+	//If we didn't use Joi to validate and check for a valid URL parameter below
+	// we could run our userCollection.find and it would be possible to attack.
+	// A URL parameter of user[$ne]=name would get executed as a MongoDB command
+	// and may result in revealing information about all users or a successful
+	// login without knowing the correct password.
+	if (validationResult.error != null) {  
+	   console.log(validationResult.error);
+	   res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+	   return;
+	}	
+
+	const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
+
+	console.log(result);
+
+    res.send(`<h1>Hello ${username}</h1>`);
+});
+
+// Array of nav links
+const navLinks = [
+    {name: "Home", link: "/main"},
+	{name: "Shop", link: "/shop"},
+    {name: "Leaderboard", link: "/leaderboard"},
+    {name: "Create Bet", link: "/createBet"},
+    {name: "Finance", link: "/money"},
+    {name: "Groups", link: "/groups"},
+    {name: "Profile", link: "/userprofile"},
+    {name: "Cat", link: "/cat"},
+]
+
+// Middleware to set nav links in locals
+app.use((req, res, next) => {
+    app.locals.navLinks = navLinks;
     next();
 });
+
+// Rendering pages
+app.get('/', (req, res) => {
+    res.render("main");
+});
+
+app.get('/main', (req, res) => {
+    res.render("main");
+});
+
+app.get('/shop', (req, res) => {
+    res.render("shop");
+});
+
+app.get('/leaderboard', (req, res) => {
+    res.render("leaderboard");
+});
+
+app.get('/createBet', (req, res) => {   
+    res.render("createBet");
+});
+
+app.get('/money', (req, res) => {
+    res.render("money");
+});
+
+app.get('/groups', (req, res) => {
+    res.render("groups");
+});
+
+app.get('/userprofile', (req, res) => {
+    res.render("userprofile");
+});
+// Rendering pages END
+
+// Absolute routing
+app.use(express.static(__dirname + "/public"));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+
+app.get(/(.*)/, (req, res, next) => {
+    res.status(404);
+	res.render("404", {navLinks: navLinks});
+    next();
+});
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
