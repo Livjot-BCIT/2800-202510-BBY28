99e1769b898cda25feaa9fa51c6d7ccddf54f397
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2025-05-16 15:59:28 -0700

    Fixed some git conflicts

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-// databaseConnection.js
-
-require("dotenv").config();
-const { MongoClient } = require("mongodb");
-
-async function connectToDatabase() {
-    try {
-        const mongodb_host = process.env.MONGODB_HOST;
-        const mongodb_user = process.env.MONGODB_USER;
-        const mongodb_password = process.env.MONGODB_PASSWORD;
-        const mongodb_database = process.env.MONGODB_DATABASE;
-
-        const uri = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}?retryWrites=true&w=majority`;
-
-        const client = new MongoClient(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-        await client.connect();
-        console.log("✅ Connected to the database successfully");
-
-        const db = client.db(mongodb_database);
-        return db;
-    } catch (error) {
-        console.error("❌ Database connection error:", error);
-        process.exit(1); // Exit the process if the connection fails
-    }
-}
-
-module.exports = { connectToDatabase };
-
-=======
 require('dotenv').config();
 
 const mongodb_host = process.env.MONGODB_HOST;
@@ -42,4 +8,3 @@ const MongoClient = require("mongodb").MongoClient;
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
 var database = new MongoClient(atlasURI, {});
 module.exports = {database};
->>>>>>> trey_tarrayo_login
