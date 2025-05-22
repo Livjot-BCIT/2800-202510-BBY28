6f6ab2ff54da6e1ed6d3490c37579b08cf0de5ad
Trey Tarrayo <rtarrayo2@my.bcit.ca>
2025-05-22 12:35:13 -0700

    Added website theme to authentication pages

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,31 +1,3 @@
-// // require("./utils.js");
-
-// require('dotenv').config();
-// const express = require('express');
-// const session = require('express-session');
-// const MongoStore = require('connect-mongo');
-// const bcrypt = require('bcrypt');
-// const saltRounds = 12;
-
-// const port = process.env.PORT || 3000;
-
-// const app = express();
-
-// const Joi = require("joi");
-
-
-// /* Secret information section */
-// const mongodb_host = process.env.MONGODB_HOST;
-// const mongodb_user = process.env.MONGODB_USER;
-// const mongodb_password = process.env.MONGODB_PASSWORD;
-// const mongodb_database = process.env.MONGODB_DATABASE;
-// const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
-// const node_session_secret = process.env.NODE_SESSION_SECRET;
-
-//Realized that this stuff is not needed in this file,
-//but I'll keep it in here for reference
-
 //Logging in
 function login() {
     const email = document.getElementById("email").value;
